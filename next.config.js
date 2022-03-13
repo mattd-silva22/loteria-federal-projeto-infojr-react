3185611e7d25bb1070fab7b9cc14f509ab5f48b0
Matheus Barretto <matheus.barretto2001@gmail.com>
2022-03-13 19:18:43 -0300

    remove islint por um tempo para testa host no netlify

--- next.config.js
+++ next.config.js
@@ -6,6 +6,11 @@ const nextConfig = {
 		styledComponents: true,
 		removeConsole: false,
 	},
+	eslint: {
+		// Warning: This allows production builds to successfully complete even if
+		// your project has ESLint errors.
+		ignoreDuringBuilds: true,
+	  }
 }
 
 module.exports = nextConfig
